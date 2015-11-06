e5a937cc091bd90abb11f4614dba32d19d7d1939
daolei.su <daolei3935@163.com>
2015-11-07 00:01:01 +0800

    java

---
+++ src/leetCode/Mian.java
@@ -0,0 +1,17 @@
+package leetCode;
+
+public class Mian {
+	
+	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n=(nums1.length+nums2.length)/2;
+        int a =0,b=0;
+        
+        
+        
+    }
+	
+	public static void main(String[] args){
+		
+	}
+
+}
